495a3ab7c85f06ce7d6bde65dad4d86acc6c1a37
Debanjan <debanjansaha2011@gmail.com>
2026-04-06 22:49:54 +0200

    fixed versioning to use local, added exception module to start working on exception handling

--- hotel-reservation-service/src/main/java/com/example/hotelreservation/service/ConfirmReservationService.java
+++ hotel-reservation-service/src/main/java/com/example/hotelreservation/service/ConfirmReservationService.java
@@ -1,10 +1,15 @@
 package com.example.hotelreservation.service;
 
 import com.example.hotelreservation.entities.Room;
+import com.example.hotelreservation.model.Status;
 import com.example.hotelreservation.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Slf4j
 @Service
@@ -14,7 +19,38 @@ public class ConfirmReservationService {
     private final RoomRepository roomRepository;
 
     public Room confirmReservation(Room room) {
-        log.info("inside confirmReservation repository");
+        log.info("inside confirmReservation service layer");
+
+        validate(room);
+
+
+        switch (room.getPaymentMode()) {
+            case CASH -> {
+                log.info("CASH chosen as Payment mode");
+                room.setStatus(Status.CONFIRMED);
+            }
+            case BANK_TRANSFER -> {
+                log.info("BANK_TRANSFER chosen as Payment mode");
+                room.setStatus(Status.PENDING_PAYMENT);
+            }
+            case CREDIT_CARD -> log.info("CREDIT_CARD chosen as Payment mode");
+            //TODO: Call credit-card-payment-service
+            //to retrieve the status of the payment. If credit payment is confirmed, then
+            //confirm the room else throw an error
+
+        }
+
         return roomRepository.save(room);
     }
+
+    private void validate(Room room) {
+        log.info("inside validate service layer");
+        LocalDate startDate = room.getStartDate();
+        LocalDate endDate = room.getEndDate();
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+        if(daysBetween > 30) {
+            throw new RuntimeException("Room cannot be reserved for more than 30 days");
+        }
+    }
+
 }
