60e46801737eb32688783398c5bd4cda6f69d94a
Debanjan <debanjansaha2011@gmail.com>
2026-04-06 11:55:56 +0200

    added Springboot runner and default gitignore

---
+++ src/main/java/com/example/assignmenthotelreservation/AssignmentHotelReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignmenthotelreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentHotelReservationApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AssignmentHotelReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignmenthotelreservation/AssignmentHotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmenthotelreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentHotelReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
